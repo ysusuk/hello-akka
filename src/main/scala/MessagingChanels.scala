c4b669c86d23f68a8f8d6b9f62cb0a7a689fd447
ysusuk <yurasusuk@gmail.com>
2015-09-01 14:02:23 +0200

    pipes and filters example

--- src/main/scala/MessagingChanels.scala
+++ src/main/scala/MessagingChanels.scala
@@ -4,6 +4,56 @@ import akka.actor.{ActorRef, Props, Actor}
  * Created by iurii.susuk on 28.08.2015.
  */
 
+case class ProcessIncomingOrder(orderInfo: Array[Byte])
+
+class OrderManagementSystem extends Actor {
+
+  def receive = {
+    case msg: ProcessIncomingOrder =>
+      val text = new String(msg.orderInfo)
+      println(s"OrderManagementSystem: processing unique order $text")
+      PipesAndFiltersDriver.completedStep()
+  }
+}
+
+class Deduplicator(nextFilter: ActorRef) extends Actor {
+
+  def receive = {
+    case msg: ProcessIncomingOrder =>
+      val text = new String(msg.orderInfo)
+      println(s"Deduplicator: processing $text")
+      // deduplicate
+      nextFilter ! msg
+      PipesAndFiltersDriver.completedStep()
+  }
+}
+
+class Authenticator(nextFilter: ActorRef) extends Actor {
+
+  def receive = {
+    case ProcessIncomingOrder(orderInfo) =>
+      val text = new String(orderInfo)
+      println(s"Authenticator: processing $text")
+      val orderText = text.replace("(certificate)", "")
+      nextFilter ! ProcessIncomingOrder(orderText.toCharArray().map(_.toByte))
+      // completable app: testing
+      PipesAndFiltersDriver.completedStep()
+  }
+}
+
+class Decrypter(nextFilter: ActorRef) extends Actor {
+
+  def receive = {
+    case ProcessIncomingOrder(orderInfo) =>
+      val text = new String(orderInfo)
+      println(s"Decrypter: processing $text")
+      val orderText = text.replace("(encryption)", "")
+      nextFilter ! ProcessIncomingOrder(orderText.toCharArray().map(_.toByte))
+      // completable app: testing
+      PipesAndFiltersDriver.completedStep()
+  }
+}
+
 class OrderAcceptanceEndpoint(nextFilter: ActorRef) extends Actor {
 
   def receive = {
@@ -11,29 +61,30 @@ class OrderAcceptanceEndpoint(nextFilter: ActorRef) extends Actor {
       val text = new String(rawOrder)
       println(s"OrderAcceptanceEndpoint: processing $text")
       nextFilter ! ProcessIncomingOrder(rawOrder)
-      // completable app
+      // completable app: testing
       PipesAndFiltersDriver.completedStep()
   }
 }
 
-object PipesAndFiltersDriver extends CompletableApp(9) {
+object PipesAndFiltersDriver extends CompletableApp(10) {
   val orderText = "(encryption)(certificate)<order id='123'>...<order>"
   val rawOrderBytes = orderText.toCharArray.map(_.toByte)
 
+  val empty = system.actorOf(Props[EmptyActor])
   val filter5 = system.actorOf(
     Props[OrderManagementSystem],
     "orderManagementSystem")
   val filter4 = system.actorOf(
-    Props(classOf(Deduplicator), filter5),
+    Props(classOf[Deduplicator], filter5),
     "deduplicator")
   val filter3 = system.actorOf(
-    Props(classOf(Authenticator), filter4),
+    Props(classOf[Authenticator], filter4),
     "authenticator")
   val filter2 = system.actorOf(
-    Props(classOf(Decrypter), filter3),
+    Props(classOf[Decrypter], filter3),
     "decrypter")
   val filter1 = system.actorOf(
-    Props(classOf(OrderAcceptanceEndpoint), filter2),
+    Props(classOf[OrderAcceptanceEndpoint], filter2),
     "orderAcceptanceEndpoint")
 
   filter1 ! rawOrderBytes
@@ -42,4 +93,8 @@ object PipesAndFiltersDriver extends CompletableApp(9) {
   awaitCompletion()
 
   println("PipesAndFiltersDriver: is completed.")
+}
+
+class EmptyActor extends Actor {
+  def receive = Actor.emptyBehavior
 }
