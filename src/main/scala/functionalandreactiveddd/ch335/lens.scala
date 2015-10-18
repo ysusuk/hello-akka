cb7869ec5db243671c812232c94ed23663792f5e
Yuriy Susuk <yurasusuk@gmail.com>
2015-10-18 23:16:17 +0200

    lens

--- src/main/scala/functionalandreactiveddd/ch335/lens.scala
+++ src/main/scala/functionalandreactiveddd/ch335/lens.scala
@@ -9,7 +9,16 @@ case class Address(no: String, street: String, city: String, state: String, zip:
 
 case class Customer(id: Int, name: String, address: Address)
 
-object Address {
+private[ch335] object U {
+  def compose[Outer, Inner, Value](outer: Lens[Outer, Inner], inner: Lens[Inner, Value]) = Lens[Outer, Value](
+    get = outer.get andThen inner.get,
+    set = (obj, value) => outer.set(obj, inner.set(outer.get(obj), value))
+  )
+}
+
+import U._
+
+private[ch335] object Address {
 
   val noLens = Lens[Address, String](
     get = (address: Address) => address.no,
@@ -19,10 +28,19 @@ object Address {
 
 object Customer {
 
-  val addressLens = Lens[Customer, Address](
+  private val addressLens = Lens[Customer, Address](
     get = (customer: Customer) => customer.address,
     set = (customer: Customer, newAddress: Address) => customer.copy(address = newAddress)
   )
+
+  val addressNoLens = compose(addressLens, Address.noLens)
+
+  //              ^
+  // abstracts to |
+  //  val addressNoLens = Lens[Customer, String](
+  //    get = (customer: Customer) => Address.noLens.get(addressLens.get(customer)),
+  //    set = (customer: Customer, newNo: String) => addressLens.set(customer, Address.noLens.set(addressLens.get(customer), newNo))
+  //  )
 }
 
 object Test extends App {
@@ -37,4 +55,9 @@ object Test extends App {
 
   val customerB13 = Customer.addressLens.set(customerB12, addressB13)
   println(Customer.addressLens.get(customerB13))
+
+  val customerB14 = Customer.addressNoLens.set(customerB13, "B-14")
+  println(Customer.addressLens.get(customerB14))
+  println(Customer.addressNoLens.get(customerB14))
+  println(customerB14)
 }
