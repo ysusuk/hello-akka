717da231153fe9d42631350be224d74ea581c425
Yuriy Susuk <yurasusuk@gmail.com>
2015-10-20 22:06:11 +0200

    Trading algebra

--- src/main/scala/functionalandreactiveddd/ch4/trading/Trading.scala
+++ src/main/scala/functionalandreactiveddd/ch4/trading/Trading.scala
@@ -3,6 +3,8 @@ package functionalandreactiveddd.ch4.trading
 /**
  * @author Yura.Susuk yurasusuk@gmail.com.
  */
-class Trading {
-
+trait Trading[Account, Market, Order, ClientOrder, Execution, Trade] {
+  def clientOrders: ClientOrder => List[Order]
+  def execute(market: Market, account: Account): Order => List[Execution]
+  def allocate(accounts: List[Account]): Execution => List[Trade]
 }
